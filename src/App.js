b1b121f407c9677b0b4768c10794f30714def99d
Christian <christian@qa.com>
2025-04-09 11:33:48 +0100

    added header comment

--- src/App.js
+++ src/App.js
@@ -36,6 +36,7 @@ function App() {
   return (
     <div className='header field'>
       VAT CALCULATOR - Christian's version
+      It's very nice
       <div className='colour-border'>
         <VatRateField customstyle="field" vatRateChanged={handleVatRateChanged} value={vatRate} updatePrices={updatePrices} />
         <PriceEntryField customstyle="field" label="Price excl VAT: " priceChanged={handleNetPriceChange} price={netPrice === 0.0 ? "" : netPrice} />
